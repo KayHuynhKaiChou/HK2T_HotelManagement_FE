398ade26c6d28276ee7aa9deff4db4ac5cdae247
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-06-30 10:42:35 +0700

    define hook common useEffectSkipFirstRender

---
+++ client/src/hooks/useEffectSkipFirstRender.ts
@@ -0,0 +1,20 @@
+import { DependencyList, EffectCallback, useEffect, useRef } from "react";
+
+export default function useEffectSkipFirstRender(
+    effectCallback : EffectCallback,
+    deps : DependencyList
+) {
+    const firstRender = useRef<boolean>(true);
+
+    useEffect(() => {
+        if(firstRender.current){
+            firstRender.current = false
+        }else{
+            const effectCallbackReturns = effectCallback();
+            if(effectCallbackReturns && typeof effectCallbackReturns === 'function'){
+                return effectCallbackReturns
+            }
+        }
+        
+    },deps)
+}
