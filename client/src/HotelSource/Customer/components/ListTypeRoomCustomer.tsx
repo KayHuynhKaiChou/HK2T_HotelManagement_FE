83c4caa84ab1467095d75ec1dc61a29cb663e3d0
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-09-05 09:01:55 +0700

    [HK2T_1022] implement UI home page continue

---
+++ client/src/HotelSource/Customer/components/ListTypeRoomCustomer.tsx
@@ -0,0 +1,48 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/reducers";
+import CarouselHk2t from "../../../common/Carousel/CarouselHk2t";
+import { useMemo } from "react";
+import { defaultViewDirection } from "../../../utils/constants";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+export default function ListTypeRoomCustomer() {
+    const {typeRooms, user} = useSelector<RootState , RootState>(state => state);
+
+    const ListTypeRoom = useMemo(() => {
+        return typeRooms.map(tRoom => (
+            <div 
+                className="bl_inforTypeRoom_wrap"
+            >
+                <div className="bl_inforTypeRoom">
+                    <img src={tRoom.images[0].link} alt="" />
+                    <div className="bl_content">
+                        <div className="bl_content_ttl">
+                            {`${tRoom.title} (${defaultViewDirection[tRoom.view_direction - 1]?.toLowerCase()} direction)`}
+                        </div>
+                        <div className="bl_content_wrap">
+                            <div className="bl_content_main">
+                                <p>{`Quantity adult: ${tRoom.adult_capacity} customers`}</p>
+                                <span>|</span>
+                                <p>{`Quantity kids: ${tRoom.kids_capacity} customers`}</p>
+                                <span>|</span>
+                                <p>{`Size room: ${tRoom.size} ㎡`}</p>
+                            </div>
+                            <div className="bl_detail">
+                                <b>xem chi tiết</b>
+                                <ArrowForwardIosIcon sx={{ fontSize: 13 }} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ))
+    },[typeRooms])
+
+    return (
+        <div className="bl_sliderTypeRooms_wrap">
+            <CarouselHk2t
+                children={ListTypeRoom}
+            />
+        </div>
+    )
+}
--- client/src/HotelSource/Customer/pages/HomePage.tsx
+++ client/src/HotelSource/Customer/pages/HomePage.tsx
@@ -1,13 +1,37 @@
+import { useDispatch } from "react-redux";
 import BannerCustomer from "../components/BannerCustomer";
 import IntroduceHotel from "../components/IntroduceHotel";
+import ListTypeRoomCustomer from "../components/ListTypeRoomCustomer";
 import Navbar from "../components/NavbarCustomer";
+import { useEffect } from "react";
+import { typeRoomAction } from "../../../redux/actions/typeRoom";
+import { anotherPartServices } from "../../../utils/constants";
 
 export default function HomePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(typeRoomAction.showAllTypeRoom() as any)
+  },[])
+
   return (
     <div>
       <Navbar />
       <BannerCustomer/>
       <IntroduceHotel/>
+      <div className="bl_mainInfor_wrap">
+        <ListTypeRoomCustomer/>
+        <div className="bl_introduceService">
+          {anotherPartServices.map(partService => (
+            <div 
+              className="bl_introduceService_item"
+            >
+              <img src={partService.backgroundImg} alt={partService.title} />
+              <h2>{partService.title}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
