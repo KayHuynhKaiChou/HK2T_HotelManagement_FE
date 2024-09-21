181b9f1c8b76d8f202277255267805baa387de6f
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-09-21 21:59:18 +0700

    [HK2T_1024] define Rating common

---
+++ client/src/common/Rating/RatingHk2t.tsx
@@ -0,0 +1,20 @@
+import { Rating } from "@mui/material";
+
+interface RatingHk2tProps {
+    name: "read-only" | "disabled" | "no-value";
+    value: number;
+}
+
+export default function RatingHk2t({
+    name,
+    value
+} : RatingHk2tProps) {
+    return (
+        <Rating 
+            name={name} 
+            value={name === 'no-value' ? null : value} 
+            readOnly={name === 'read-only'}
+            disabled={name === 'disabled'} 
+        />
+    )
+}
