b96facb5de0e440b4adda4640c7aa3f92805db9d
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-09-29 11:21:13 +0700

    [HK2T_1024] re-fix logic form profile common

--- client/src/common/FormProfile/FormUpdateProfile.tsx
+++ client/src/common/FormProfile/FormUpdateProfile.tsx
@@ -8,40 +8,41 @@ import SelectHk2t from '../SelectHk2t';
 import provinces from '../../data/provinces.json';
 import districts from '../../data/districts.json';
 import wards from '../../data/wards.json';
-import { forwardRef, useEffect, useImperativeHandle, useMemo } from 'react';
+import { forwardRef, useImperativeHandle, useMemo } from 'react';
 import RadioBtnHk2t from '../RadioBtnHk2t';
 import { uuid } from '../../utils';
 import { defaultGenders } from '../../utils/constants';
-import { FormUserEmployee } from '../../types/form';
+import { FormUserProfile } from '../../types/form';
 import UploadFileBtnHk2t from '../UploadFileBtnHk2t';
 import ButtonHk2t from '../ButtonHk2t';
+import useEffectSkipFirstRender from '../../hooks/useEffectSkipFirstRender';
+import { Dayjs } from 'dayjs';
 
 interface FormUpdateProfileProps {
     user : User;
-    onUpdateProfile : (values : FormUserEmployee) => void;
+    onUpdateProfile : (values : FormUserProfile) => void;
 }
 
 export interface FormUpdateProfileHandle {
-    form : UseFormReturn<FormUserEmployee>
+    form : UseFormReturn<FormUserProfile>
 }
 
 const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfileProps>((props , ref) => {
     const {user , onUpdateProfile} = props;
-
     const schema = yup.object({
         firstname: yup.string()
-                .required('Please enter firstname !'),
+        .required('Please enter firstname !'),
         surname: yup.string()
-                .required('Please enter surname !'),
+        .required('Please enter surname !'),
         email: yup.string()
-                .required('Please enter email !'),
+        .required('Please enter email !'),
         phone: yup.string()
-                .required('Please enter phone !'),
+        .required('Please enter phone !'),
         birth_day: yup.string()
-                .required('Please select date !'),
+        .required('Please select date !'),
         gender: yup.number()
-                .oneOf([1,2] as const)
-                .required(),
+        .oneOf([1,2] as const)
+        .required(),
         link_avatar: yup.string().required(),
         city: yup.object().shape({
             label: yup.string().required(),
@@ -56,21 +57,23 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
             value: yup.string().required(),
         }).required('Please select ward !'),
         address: yup.string()
-                .required('Please enter current address !'),
+        .required('Please enter current address !'),
     });
-
+    
     const defaultLocation = {
         city : {label : '---Choose province---' , value : ''},
         district : {label : '---Choose district---' , value : ''},
         ward : {label : '---Choose ward---' , value : ''},
     }
-
-    const generateFirstFormProfile = (): FormUserEmployee => {
+    
+    const generateFirstFormProfile = (): FormUserProfile => {
         const provinceName = provinces.find(p => p.province_id.toString() == user.city)?.province_name 
         const districtName = districts.find(d => d.district_id.toString() == user.district)?.district_name 
-
+        const wardName = wards.find(d => d.ward_id.toString() == user.ward)?.ward_name
         return {
-            ...user,
+            firstname : user.firstname,
+            surname : user.surname,
+            email : user.email,
             gender : user.gender || 1,
             phone : user.phone || '',
             birth_day : user.birth_day || '',
@@ -84,18 +87,19 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
                 value : user.district || defaultLocation.district.value
             },
             ward : {
-                label : user.ward || defaultLocation.ward.label, 
+                label : wardName || defaultLocation.ward.label, 
                 value : user.ward || defaultLocation.ward.value
             },
             address : user.address || ''
         }
     }
-
+    
     // hook form
-    const form : UseFormReturn<FormUserEmployee> = useForm({
+    const form : UseFormReturn<FormUserProfile> = useForm({
         defaultValues: generateFirstFormProfile(),
+        // @ts-ignore
         resolver: yupResolver(schema)
-    }) as UseFormReturn<FormUserEmployee>
+    }) as UseFormReturn<FormUserProfile>
 
     // useImperativeHandle hook
     useImperativeHandle(ref , () => ({ form }))
@@ -105,6 +109,7 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
     const uploadedLinkImage = form.watch("link_avatar" , '');
     const selectedCity = form.watch("city" , {label : '' , value : ''});
     const selectedDistrict = form.watch("district" , {label : '' , value : ''});
+    const selectedWard = form.watch("ward" , {label : '' , value : ''});
 
     // useMemo hook
     const optionProvinces = useMemo(() => {
@@ -132,7 +137,7 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
             .filter((w) => w["district_id"].toString() == selectedDistrict.value)
             .map((w) => ({
                 label : w["ward_name"],
-                value : w["ward_name"]
+                value : w["ward_id"].toString()
             }))
         updatedWards.unshift(defaultLocation.ward)
         return selectedDistrict.value 
@@ -141,26 +146,22 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
     },[selectedDistrict])
 
     // useEffect hook
-    useEffect(() => {
+    useEffectSkipFirstRender(() => {
         const district = districts.find(d => d.district_id.toString() == selectedDistrict.value)
         if(district?.province_id.toString() !== selectedCity.value){
             form.setValue("district" , {label : '---Choose district---' , value : ''})
         }
     },[selectedCity])
 
-    useEffect(() => {
-        const ward = wards.find(w => w.ward_name == form.getValues('ward').value)
+    useEffectSkipFirstRender(() => {
+        const ward = wards.find(w => w.ward_id.toString() == selectedWard.value)
         if(ward?.district_id.toString() !== selectedDistrict.value){
             form.setValue("ward" , {label : '---Choose ward---' , value : ''})
         }
     },[selectedDistrict])
 
-    useEffect(() => { // khi render lần 1 thì chưa call api get profile , do đó lần 2 mới set form
-        form.reset(generateFirstFormProfile())
-    },[user])
-
     // func change state form
-    const handleChangeGender = (gender : FormUserEmployee['gender']) => {       
+    const handleChangeGender = (gender : FormUserProfile['gender']) => {       
         form.setValue("gender", gender)
     }
 
@@ -168,9 +169,13 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
         form.setValue("link_avatar", images[0] + '')
     }
 
+    const handleChangeDatePicker = (dateRange: Dayjs | null) => {
+        dateRange && form.setValue("birth_day", dateRange.format('YYYY-MM-DD'))
+    }
+
     return (
         <form 
-            onSubmit={form.handleSubmit(onUpdateProfile as SubmitHandler<FormUserEmployee>)}
+            onSubmit={form.handleSubmit(onUpdateProfile as SubmitHandler<FormUserProfile>)}
             className='bl_personInfor_form'
         >
             <div className="bl_personInfor_form_inner">
@@ -235,7 +240,8 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
                                 name='birth_day' 
                                 typeInput='date' 
                                 placeholder='select birthdate' 
-                                form={form} 
+                                form={form}
+                                onChangeDatePicker={handleChangeDatePicker}
                             />
                         </Grid>
                         <Grid item sm={6} container alignItems={"center"}>
@@ -249,7 +255,7 @@ const FormUpdateProfile = forwardRef<FormUpdateProfileHandle , FormUpdateProfile
                                 Gender
                             </Grid>
                             {defaultGenders.map((gender , index) => {
-                                const value = index + 1 as FormUserEmployee['gender']
+                                const value = index + 1 as FormUserProfile['gender']
                                 return (
                                     <Grid>
                                         <RadioBtnHk2t
