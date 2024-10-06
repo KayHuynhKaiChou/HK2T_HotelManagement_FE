27f21508336da246d777e624d95bd37565115e40
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-10-06 21:27:52 +0700

    [HK2T_1035] remove func changeDate

--- client/src/common/InputHk2t.tsx
+++ client/src/common/InputHk2t.tsx
@@ -138,7 +138,7 @@ function InputHk2t(props : InputProps) {
                 {label}
             </div>
             {
-                typeInput === 'date' && onChangeDatePicker 
+                typeInput === 'date' 
                     ? renderDatePicker() 
                     : renderInput()
             }
