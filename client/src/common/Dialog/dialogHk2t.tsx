f060c1e7729095896d9da77882d90caa47444bfb
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-10-20 16:09:44 +0700

    [HK2T_1024] coding common dialog

---
+++ client/src/common/Dialog/dialogHk2t.tsx
@@ -0,0 +1,99 @@
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { createContext, ReactNode, useCallback, useContext, useRef, useState } from "react";
+import { noop } from "../../utils/noop";
+import ButtonHk2t from "../ButtonHk2t";
+import { colorsBtnCustom } from "../../utils/constants";
+
+type TypeDialog = 'CONFIRM' | 'ERROR'
+
+type DialogContextProps = {
+    show: (comp: JSX.Element, callback?: () => void, typeDialog?: TypeDialog) => void;
+    hide: (callback?: () => void) => void
+}
+
+const defaultProps = {
+    show: () => {},
+    hide: () => {}
+}
+
+export const DialogHk2tContext = createContext<DialogContextProps>(defaultProps)
+
+export const DialogHk2tProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [open, setOpen] = useState(false);
+    const [contentComp, setContentComp] = useState(<></>);
+    const [typeDialog, setTypeDialog] = useState<TypeDialog>('ERROR')
+    const ref = useRef<() => void>()
+
+    const show : DialogContextProps['show'] = useCallback(
+        (comp, callback, type) => {
+            type && type !== typeDialog && setTypeDialog(type)
+            setOpen(true)
+            setContentComp(comp)
+            ref.current = callback || noop
+        },
+        [open]
+    )
+
+    const hide : DialogContextProps['hide'] = useCallback(
+        (callback) => {
+            setOpen(false)
+
+            if(callback && typeof callback === 'function'){
+                callback()
+                ref.current = noop
+            } else if (ref.current){
+                ref.current()
+            }
+        },
+        [open]
+    )
+
+    const cancel = () => {
+        setOpen(false)
+        ref.current = noop
+    }
+
+    return (
+        <DialogHk2tContext.Provider value={{ show, hide }}>
+            {children}
+            <Dialog
+                open={open}
+                fullWidth
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogContent dividers>
+                    <DialogContentText 
+                        id="alert-dialog-description"
+                    >
+                        {contentComp}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    {typeDialog === 'ERROR' ? (
+                        <ButtonHk2t
+                            content='OK'
+                            onClick={() => hide()}
+                        />
+                    ) : (
+                        <>
+                            <ButtonHk2t
+                                content='OK'
+                                onClick={() => hide()}
+                            />
+                            <ButtonHk2t
+                                colorCustom={colorsBtnCustom['danger']}
+                                content='CANCEL'
+                                onClick={() => cancel()}
+                            />
+                        </>
+                    )}
+                </DialogActions>
+            </Dialog>
+        </DialogHk2tContext.Provider>
+    )
+}
+
+export function useDialogHk2t(): DialogContextProps {
+    return useContext(DialogHk2tContext)
+}
