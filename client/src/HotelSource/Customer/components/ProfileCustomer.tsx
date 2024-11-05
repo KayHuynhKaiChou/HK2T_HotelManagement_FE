22ebd519c73032ebea92782570ba6ca09acd7421
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-11-05 16:57:24 +0700

    [HK2T_1025] fix bug payload format profile

--- client/src/HotelSource/Customer/components/ProfileCustomer.tsx
+++ client/src/HotelSource/Customer/components/ProfileCustomer.tsx
@@ -1,19 +1,21 @@
 import { useSelector } from "react-redux";
-import LoadingHk2t from "../../../common/LoadingHk2t";
-import { useEffect, useRef } from "react";
+import LoadingHk2tComponent from "../../../common/Loading/LoadingHk2tComponent";
+import { useRef } from "react";
 import { RootState } from "../../../redux/reducers";
 import { useDispatch } from "react-redux";
 import FormUpdateProfile, { FormUpdateProfileHandle } from "../../../common/FormProfile/FormUpdateProfile";
 import { userAction } from "../../../redux/actions/user";
-import { User } from "../../../types/models";
 import { FormUserProfile } from "../../../types/form";
-import { toastMSGObject } from "../../../utils";
+import { formatUpdatedProfile, toastMSGObject } from "../../../utils";
 import { toast } from "react-toastify";
 import useEffectSkipFirstRender from "../../../hooks/useEffectSkipFirstRender";
 
 export default function ProfileCustomer() {
     //redux
     const {user , response} = useSelector<RootState , RootState>(state => state);
+    const profileClone = {...user};
+    delete profileClone.token;
+    delete profileClone._persist;
     const dispatch = useDispatch();
 
     //ref
@@ -21,14 +23,8 @@ export default function ProfileCustomer() {
 
     //func handle submit form
     const handleUpdateProfile = (updatedProfile : FormUserProfile) => {
-        const formatUpdatedProfile : User = {
-            ...updatedProfile,
-            link_avatar : updatedProfile.link_avatar.replace('data:', '').replace(/^.+,/, ''),
-            city : updatedProfile.city.value + '',
-            district : updatedProfile.district.value + '',
-            ward : updatedProfile.ward.value + ''
-        }
-        dispatch(userAction.updateUser(formatUpdatedProfile) as any)
+        const payloadResult = formatUpdatedProfile(updatedProfile)
+        dispatch(userAction.updateUser(payloadResult) as any)
     }
 
     //useEffect 
@@ -43,11 +39,11 @@ export default function ProfileCustomer() {
         <div className="bl_profile">
             <div className="bl_profile_inner">
                 {response.isLoading && (
-                    <LoadingHk2t/>
+                    <LoadingHk2tComponent/>
                 )}               
                 <FormUpdateProfile
                     ref={formUpdateProfile}
-                    user={user}
+                    user={profileClone}
                     onUpdateProfile={handleUpdateProfile}
                 />
             </div>
