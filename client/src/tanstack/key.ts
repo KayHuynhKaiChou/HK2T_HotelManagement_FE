0c2aa6f9c14a8b53ed32592ca231ee100f548b0e
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-11-05 16:45:21 +0700

    [HK2T_1024] constanst status and key use query

---
+++ client/src/tanstack/key.ts
@@ -0,0 +1,11 @@
+import { uuid } from "../utils"
+
+const queryKeyAllUser = `all-user-${uuid()}`
+const queryKeyAllReversation = `all-reversation-${uuid()}`
+const queryKeyAllRoom = `all-room-${uuid()}`
+
+export {
+    queryKeyAllUser,
+    queryKeyAllReversation,
+    queryKeyAllRoom
+}
