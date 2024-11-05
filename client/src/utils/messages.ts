db601aaf1674ffeaa2ba263a80ce29171fda3f96
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2024-11-05 16:49:03 +0700

    [HK2T_1031] add message for reservation action

--- client/src/utils/messages.ts
+++ client/src/utils/messages.ts
@@ -15,6 +15,25 @@ export const MESSAGE = {
             'FAIL': 'Account change status failed !'
         }
     },
+    AMENITY : {
+        'CREATE': getMsgNotification('create', 'amenity')
+    },
+    TYPE_ROOM : {
+        'CREATE': getMsgNotification('create', 'type room'),
+        'UPDATE': getMsgNotification('update', 'type room'),
+    },
+    REVERSATION : {
+        'CREATE': getMsgNotification('create', 'reversation'),
+        'UPDATE': getMsgNotification('update', 'reversation'),
+        'NO_AVAILABLE_ROOM': 'This room type is currently fully booked, please change to another room type.',
+        'STATUS_OPEN': {
+            'CANCEL_SUCCESS' : 'You have successfully canceled, please contact admin via phone number to return the deposit.'
+        },
+        'STATUS_IN_PROGRESS': {
+            'CONFIRM_CANCEL' : 'Are you sure you want to cancel, if you agree you will lose your deposit.',
+            'CANCEL_SUCCESS' : 'You have successfully canceled !'
+        }
+    },
     CONFIRM_SUBMIT : 'Your changes have not summited yet, you still wanna change to another part'
     
 }
